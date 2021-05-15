cdc038ab214030c54ac1dcae12fe0600d8e91970
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-05-15 13:27:29 +0100

    Initial Commit

---
+++ Project2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2
+//
+//  Created by MAC on 15/5/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
